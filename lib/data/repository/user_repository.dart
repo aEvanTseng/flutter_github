e3c8e7b04dfc2da508ecec8045ffaf8739777b44
Evan Tseng <evan.tseng@access-company.com>
2022-09-14 16:22:25 +0800

    update token

--- lib/data/repository/user_repository.dart
+++ lib/data/repository/user_repository.dart
@@ -1,12 +1,12 @@
 import 'dart:convert';
-
+import 'package:flutter_github/config.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_github/data/model/user.dart';
 
 class UserRepository {
   final client = http.Client();
-  static const String githubToken = "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";
+  static const githubToken = Config.githubToken;
 
   Future<UserList> getUsers() async {
     final response = await client.get(_buildUri(endpoint: "/users"),
